07dc20d85b68b5f7fd8a6e4668440734a86b9f9a
smukvica <36771790+smukvica@users.noreply.github.com>
2018-12-01 23:24:43 +0100

    Add files via upload

--- Naloga4.java
+++ Naloga4.java
@@ -1,338 +1,207 @@
+
 import java.io.*;
-import java.util.Scanner;
 
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Mitja Vendramin
+ */
 public class Naloga4{
-     public static void main(String[] args) throws IOException{
-        
-            //String dat = args[0];
-            //BufferedReader br = null;
-            //File vhod = new File(dat);
-            //br = new BufferedReader(new FileReader(vhod));
-            Scanner sc = new Scanner(System.in);
-            //int stUkazov = Integer.parseInt(br.readLine());
-            Naloga4 list = new Naloga4();
-            int count = 0;
-            int stUkazov = sc.nextInt();
-            while(stUkazov != 0){
-                
-                String preberi = sc.nextLine();
-                String[] vrstica =  preberi.split(",");
-                String ukaz = vrstica[0];
 
-                switch(ukaz){
-                    case "s": 
-                        int velikost = Integer.parseInt(vrstica[1]);     
-                        list.addFirst(velikost);
-                         break;
-                         
-                    case "i":
-                        int vrednost = Integer.parseInt(vrstica[1]);
-                        int pozicija = Integer.parseInt(vrstica[2]);
-                        list.insert(vrednost, pozicija);
-                        //System.out.println("Tabele:"+count);
-                        count++;
-                        //list.tabele();
+    public static void main(String [] args){
+        int count = 0;
+        try{
+            File file = new File(args[0]);
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            int stevilo = Integer.parseInt(br.readLine());
+            clen c = new clen();
+            
+            while(stevilo > 0){
+                String s = br.readLine();
+                String [] k = s.split(",");
+                switch(k[0]){
+                    case "s":{
+                        c.init(Integer.parseInt(k[1]));
                         break;
-                        
-                    case "r":
-                        int poz = Integer.parseInt(vrstica[1]);
-                        //System.out.println("Tabele:"+count);
-                        count++;
-                        
-                        list.remove(poz);
-                        //list.tabele();
+                    }
+                    case "i":{
+                        c.insert(Integer.parseInt(k[1]), Integer.parseInt(k[2]));
+                        break;
+                    }
+                    case "r":{
+                        c.remove(Integer.parseInt(k[1]));
+                        break;
+                    }
                 }
-                 stUkazov--;
+                stevilo--;
+                count++;
             }
-            //System.out.println(list.size);
-            list.tabele();
+            PrintWriter pw = new PrintWriter(args[1]);
+            pw.write(c.izpis());
+            pw.close();
             
-            //br.close();
-            
-            //int tabelaZasedenosti[] = list.zasedenost();
-            //int[] izpis = new int[velikost+1];
-            
-            /*for(int i = 0;i<tabelaZasedenosti.length;i++){
-                   izpis[(velikost-tabelaZasedenosti[i])]++;
-            }*/
-            
-         /*
-         //TESTIRANJE
-         Naloga4 list = new Naloga4();
-         
-         list.addFirst(5);
-         boolean test = list.insert(7, 0);
-         list.insert(3, 1);
-         list.insert(4, 0);
-         list.insert(2,3);
-         list.insert(1, 4);
-         list.insert(5, 3);
-         list.insert(8, 2);
-         list.remove(0);
-         list.remove(1);
-         list.remove(3);
-         list.remove(1);
-         //System.out.println("Je nrdilo ? "+test);
-         
-        list.tabele();
-         */
-     }
-
-Node root;
-int velikost;
-int size = 0;
-public Naloga4(){
-    root = new Node();
+        }catch(Exception e){
+            System.out.println(e);
+            System.out.println(count);
+        }
+    }
 }
+class clen{
+    
+    clen naslednji = null;
+    int [] elementi;
+    int prost;
+    int st;
+    
+    
+    public void init(int n){
+        elementi = new int [n];
+        prost = n;
+        st = n;
+    }
+    
+    public boolean insert(int v, int p){
+        if(p > st-prost){
+            naslednji.insert(v, p-(st-prost));
+        }
+        else{
+            if(elementi[p] == 0){
+                elementi[p] = v;
+                prost--;
+                return true;
+            }else{
+                if(prost > 0){
+                    shiftR(p);
+                    elementi[p] = v;
+                    prost--;
+                    return true;
+                }else{
+                    if(naslednji == null){
+                        naslednji = new clen();
+                        naslednji.init(st);
+                    }else{
+                        clen t = naslednji;
+                        naslednji = new clen();
+                        naslednji.init(st);
+                        naslednji.naslednji = t;
 
-public void tabele(){
-    Node thisNode = this.root;
-    
-    
-    while(thisNode != null){
-        //System.out.println("Tabela:");
-        int[] tabela = thisNode.getPolje();
-        for(int i = 0; i < tabela.length;i++){
-            if(i == tabela.length-1){
-                if(tabela[i] == 0)
-                    System.out.print("NULL");
-                else
-                    System.out.print(tabela[i]);
-            }
-            else{
-                if(tabela[i]==0)
-                    System.out.print("NULL,");
-                else
-                    System.out.print(tabela[i]+",");
+                    }
+                    int a = st/2;
+                    for(int i = a; i < st; i++){
+                        naslednji.insert(elementi[i], i-a);
+                        elementi[i] = 0;
+                        prost++;
+                    }
+                    if(p <= a){
+                        this.insert(v, p);
+                    }else{
+                        naslednji.insert(v, p-a);
+                    }
+                }
             }
         }
-        thisNode = thisNode.getNaslednji();
-        System.out.println("");
+        return false;
     }
     
-}
-
-public Node addFirst(int space){
-    Node newNode = new Node(space,root);
-    this.root = newNode;
-    velikost = space;
-    size++;
-    return newNode;
-}
-
-public boolean remove(int pozicija){
-    
-    Node thisNode = this.root;
-    Node naslednji = thisNode.getNaslednji();
-    while(pozicija > thisNode.getZaseden()){
-        //System.out.println("Tale je zaseden "+thisNode.getZaseden());
-        pozicija -= thisNode.getZaseden();
-        thisNode = thisNode.getNaslednji(); 
-        naslednji = thisNode.getNaslednji();
-        //System.out.println("Tle je pozicija "+pozicija);
-    }
-    boolean rezanje = thisNode.odstraniVrednost(pozicija);
-    //System.out.println("Rezanje"+rezanje);
-    
-    if(rezanje == false){
-        int temp[] = naslednji.getPolje();
-        //System.out.println(temp[1]);
-        for(int i = 0; i < temp.length; i++){
-            if(temp[i] == 0)
-                break;
-            thisNode.dodajVrednost(temp[i], thisNode.zaseden);
-            //System.out.println("Dodamo "+temp[i]);
+    public boolean remove(int p){
+        if(p > st - 1 - prost){
+            naslednji.remove(p - (st-prost));
         }
-        if(thisNode.getNaslednji() == null)
-            naslednji.setNaslednji(null);
-        else
-            naslednji.setNaslednji(thisNode.getNaslednji());
-        size--;
-    }
-    
-    else if(rezanje == true)
-        return true;
-    
-    
-    return false;
-}
-
-public boolean insert(int vrednost, int pozicija){
-    Node thisNode = this.root;
-    int vstavljanje = pozicija;
-
-    while(vstavljanje > thisNode.getZaseden()){
-        //System.out.println("Tale je zaseden "+thisNode.getZaseden());
-        vstavljanje -= thisNode.getZaseden();
-        thisNode = thisNode.getNaslednji();    
-    }
-    if(thisNode.getFree() > 0){
-        //System.out.println(thisNode.getFree());
-        //System.out.println("Vstavljamo "+vrednost+" na pozicijo "+vstavljanje);
-        thisNode.dodajVrednost(vrednost, vstavljanje);
+        else{
+            elementi[p] = 0;
+            prost++;
+            if(prost != st){
+                this.shiftL(p);
+            }
+            if(prost > st/2 + 1 && naslednji != null){
+                elementi[st/2 - 1] = naslednji.prvi();
+                naslednji.shiftL(0);
+                naslednji.prost++;
+                if(naslednji.prost == st){
+                    naslednji = naslednji.naslednji;
+                }
+                prost--;
+            }
+        }
+//        else{
+//            if(elementi[p] == 0){
+//                if(naslednji != null){
+//                    int a = st/2;
+//                    naslednji.remove(p - a);
+//                    if(naslednji.prost == st){
+//                        naslednji = naslednji.naslednji;
+//                    }
+//                }else{
+//                    return false;
+//                }
+//            }else{
+//                elementi[p] = 0;
+//                prost++;
+//                if(prost != st){
+//                    this.shiftL(p);
+//                }
+//                if(prost > st/2 + 1 && naslednji != null){
+//                    elementi[st/2 - 1] = naslednji.prvi();
+//                    naslednji.shiftL(0);
+//                    naslednji.prost++;
+//                    if(naslednji.prost == st){
+//                        naslednji = naslednji.naslednji;
+//                    }
+//                    prost--;
+//                }
+//            }
+//        }
+        
         return true;
     }
-   
-    if(thisNode.getFree() <= 0){
-        int[] tabelaClena = thisNode.getPolje();
-        
-
-        Node newNode = new Node(velikost,thisNode.getNaslednji());
-        
-        for(int i = velikost; i > (velikost/2); i--){    
-            newNode.dodajVrednost(tabelaClena[i-1], 0);
-            tabelaClena[i-1] = 0;
+    
+    public void shiftL(int a){
+        for(int i = a; i < st-1; i++){
+            elementi[i] = elementi[i+1];
         }
-        thisNode.setPolje(tabelaClena);
-        thisNode.setZaseden(velikost/2);
-        
-        
-        
-      
-        
-        //System.out.println("Dodali smo nov člen");
-        //System.out.println("Tabela prvega ");
-        for(int i = 0; i < velikost; i++){
-            //System.out.println(thisNode.getPolje()[i]);
-        }
-        //System.out.println("Tabela novega");
-        for(int i = 0; i < velikost; i++){
-            //System.out.println(newNode.getPolje()[i]);
-        }
-        
-        
-        if(thisNode.getNaslednji() == null)
-            thisNode.setNaslednji(newNode);
-        else{
-            newNode.setNaslednji(thisNode.getNaslednji());
-            thisNode.setNaslednji(newNode);
-        }
-        
-        size++;
-        //System.out.println("Pozicija "+pozicija);
-        return insert(vrednost,pozicija);
+        elementi[st-1] = 0;
     }
     
-    
-    return false;
-}
-
-class Node{
-    Node nextNode;
-    int free;
-    int zaseden;
-    int tabela[];
-    int velikost;
-    
-    Node(){
+    public void shiftR(int a){
+        for(int i = st-1; i > a; i--){
+            elementi[i] = elementi[i-1];
+        }
     }
     
-    Node(int velikost, Node naslednji){
-        this.free = velikost;
-        this.nextNode = null;
-        this.zaseden = 0;
-        this.tabela = new int[velikost];
-        this.velikost = velikost;
+    public int prvi(){
+        return elementi[0];
     }
     
-    boolean odstraniVrednost(int pozicija){
-        
-        /*System.out.println("tabela pred:");
-        for(int i = 0; i<tabela.length; i++){
-             System.out.print(tabela[i]);
+    public String izpis(){
+        int a = 1;
+        clen t = naslednji;
+        while(t != null){
+            t = t.naslednji;
+            a++;
         }
-        System.out.println("");*/
-        tabela[pozicija] = 0;
-        free +=1;
-        zaseden -= 1;
-        int[] temp = tabela;
-        //System.out.println("Odstranimo clen na poziciji "+pozicija);
-        //int count = 0;
-        int swap = 0;
-        
-        for(int i = 0; i < tabela.length; i++){
-            if(temp[i]!=0){
-                tabela[swap] = temp[i];
-                swap++;  
-                }
-        }
-
-        if(nextNode != null && zaseden < velikost/2){
-            int prvi = nextNode.vrniPrvega();
-            dodajVrednost(prvi,zaseden);
-            return nextNode.odstraniVrednost(0);
-        }
-        if(nextNode == null && zaseden < velikost/2){
-            //System.out.println("pride sm in vrne false ?");
-            tabela[zaseden] = 0;
-            return false;
-        }
-        
-        else 
-            tabela[zaseden] = 0;
-            return true;
-        
-           
-        
-       /* System.out.println("Tabela po:");
-        for(int i = 0; i<tabela.length; i++){
-             System.out.print(tabela[i]);
-        }
-        System.out.println("");*/
-
+        return "" + a + "\n" +izpis2();
     }
-
-    void dodajVrednost(int vrednost, int pozicija){
-        
-        if(tabela[pozicija] == 0)
-            tabela[pozicija] = vrednost;
-        else{
-            int temp = tabela[pozicija];
-            for(int i = pozicija; i < tabela.length-1; i++){  
-                int temp1 = tabela[i+1];
-                tabela[i+1] = temp;
-                temp = temp1; 
-                //System.out.println("Vrednost "+temp+" premaknemo na "+(i+1));     
+    
+    public String izpis2(){
+        String s = "";
+        for(int i = 0; i < st; i++){
+            if(elementi[i] == 0){
+                s += "NULL,";
+            }else{
+                s += elementi[i] + ",";
             }
-            tabela[pozicija] = vrednost;
         }
+        s = s.substring(0, s.length()-1);
         
-        this.free -= 1;
-        this.zaseden += 1;
-        
-       // System.out.println("Vrednost dodana na mesto "+pozicija);
-        
-        /*for(int i = 0; i<tabela.length;i++){
-            System.out.println(tabela[i]);
-        }*/
-        
+        if(naslednji != null){
+            s += "\n" + naslednji.izpis2();
+        }
+        return s;
     }
     
-    int vrniPrvega(){
-        return tabela[0];
-    }
-    void setNaslednji(Node naslednji){
-        this.nextNode = naslednji;
-    }
-    Node getNaslednji(){
-        return this.nextNode;
-    }
-    int getZaseden(){
-        return zaseden;
-    }
-    int getFree(){
-        return free;
-    }
-    void setZaseden(int zaseden){
-        this.zaseden=zaseden;
-        this.free = velikost - zaseden;
-    }
-    void setPolje(int[] polje){
-        tabela = polje;
-    }
-    int[] getPolje(){
-        return tabela;
-    }
-    
-}
 }
